771c61cdff380782f1de6b77d5521734b5cba854
Gustavo Falco <comfortablynumb84@gmail.com>
2015-12-28 12:23:37 -0300

    Added basic files.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cache
+# Cache
+
 Cache component which serves as a factory of doctrine cache component classes.
---
+++ src/Factory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the frenzy-framework package.
+ *
+ * (c) Gustavo Falco <comfortablynumb84@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IronEdge\Component\Cache;
+
+
+/*
+ * @author Gustavo Falco <comfortablynumb84@gmail.com>
+ */
+class Factory
+{
+    /**
+     * Cache instances created by this factory.
+     *
+     * @var array
+     */
+    private $_instances = [];
+
+
+    public function create($id, $type, array $config)
+    {
+
+    }
+}
